ae359cc54a098f33ba2c925a0c006b778455589a
Guilherme Duarte <guilhduart.abr@gmail.com>
2025-12-19 22:36:07 -0300

    feat: redis logs

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,12 @@ import { BullAdapter } from '@bull-board/api/bullAdapter';
 import { getQueueToken } from '@nestjs/bull';
 import { createBullBoard } from '@bull-board/api';
 import { Queue } from 'bull';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const logger = new Logger('Bootstrap');
 
   app.enableCors({
     origin: true,
@@ -27,19 +30,28 @@ async function bootstrap() {
 
   const eprocQueue = app.get<Queue>(getQueueToken('eproc-process-queue'));
   const esajQueue = app.get<Queue>(getQueueToken('esaj-process-queue'));
+
   const serverAdapter = new ExpressAdapter();
   serverAdapter.setBasePath('/admin/queues');
+
   createBullBoard({
     queues: [new BullAdapter(eprocQueue), new BullAdapter(esajQueue)],
     serverAdapter,
   });
   app.use('/admin/queues', serverAdapter.getRouter());
 
+  const client = eprocQueue.client;
+
+  client.on('ready', () => logger.log('Redis ready ✅'));
+  client.on('error', (err) => logger.error('Redis error ❌', err));
+  client.on('end', () => logger.warn('Redis connection closed ⚠️'));
+
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api', app, document);
 
   await app.listen(port);
+
   console.log(`Swagger API disponível em: http://localhost:${port}/api`);
   console.log(
     `Bull Board disponível em: http://localhost:${port}/admin/queues`,
